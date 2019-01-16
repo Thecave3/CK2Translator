3fc553d8e199cc7e1a761c6aabbdae9b4e656ced
Andrea Lacava <thecave003@gmail.com>
2019-01-16 20:32:04 +0100

    aggiunto nuovo file txt localizator

--- txt_localizator.py
+++ txt_localizator.py
@@ -1,14 +1,37 @@
-# dati i tre file ITA.csv, SPA.csv e 00_customizable_localisation_IT.txt, genero un file di nome
-# 00_customizable_localisation_IT_result.txt avente al posto delle righe della colonna 0 di SPA.csv le righe della
-# colonna 0 di ITA.csv per far si che ciò accada correttamente le righe di ITA.csv e quelle di SPA.csv devono essere
-# una la traduzione dell'altra (riga 0 colonna 0 di ITA.csv corrisponde alla traduzione di riga 0 colonna 0 di SPA.csv)
-
+# dati i file dictionary.csv e 00_customizable_localisation_IT.txt, genero un file di nome
+# 00_customizable_localisation_IT_result.txt avente le righe della colonna 0 di dictionary.csv invece che le righe
+# della colonna 3 di dictionary.csv.  per far si che ciò accada correttamente la riga 0 colonna 0 deve corrispondere
+# alla traduzione di riga 0 colonna 3
 working_directory = "A:\\Download\\CK2Translator\\Lingua\\"
 working_subdirectory = "customizable_localisation\\"
-spa_filename = "SPA.csv"
-ita_filename = "ITA.csv"
-source_filename = ""
+dictionary_filename = "dictionary.csv"
+source_filename = "00_customizable_localisation_IT.txt"
+
+source = working_directory + working_subdirectory + source_filename
 
 destination_path = "A:\\Download\\CK2Translator output\\"
 destination_name = "00_customizable_localisation_IT_result.txt"
 destination = destination_path + destination_name
+
+translation_table = {}
+
+dictionary = open(working_directory + dictionary_filename, 'r', encoding='ansi')
+
+for line in dictionary:
+    linea = line.strip('\n').split(';')
+    if len(linea) > 4:
+        translation_table[linea[3]] = linea[0]
+
+source = open(source, 'r', encoding='ansi')
+output_file = open(destination, 'w', encoding='ansi')
+
+for line in source.readlines():
+    new_line = ""
+    for key in translation_table.keys():
+        if key in line:
+            new_line = line.replace(key, translation_table[key])
+    output_file.write(new_line)
+
+dictionary.close()
+source.close()
+output_file.close()
